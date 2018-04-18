ff2b3d1b7be17de7591b0737a7fe7b8e2dac2896
Sree Harsha Konduri <srkonduri@expedia.com>
2018-04-18 02:33:48 -0700

    create a pull consumer based buffer for ordered messaging

--- example/src/main/java/org/apache/rocketmq/example/simple/PullConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/simple/PullConsumer.java
@@ -16,32 +16,72 @@
  */
 package org.apache.rocketmq.example.simple;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
 import org.apache.rocketmq.client.consumer.PullResult;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageQueue;
 
 public class PullConsumer {
     private static final Map<MessageQueue, Long> OFFSE_TABLE = new HashMap<MessageQueue, Long>();
 
     public static void main(String[] args) throws MQClientException {
-        DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("please_rename_unique_group_name_5");
-
+        DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("g2");
+        List<Message> messages = new ArrayList<>();
+        Map<Integer,Message> buffer = new HashMap<>();
+        int expectedMessage = 0;
         consumer.start();
 
-        Set<MessageQueue> mqs = consumer.fetchSubscribeMessageQueues("TopicTest1");
+        Set<MessageQueue> mqs = consumer.fetchSubscribeMessageQueues("tp4");
+//        while (true) {
+//            mqs.forEach((mq) -> {
+//                try {
+//                    PullResult pullResult =
+//                        consumer.pull(mq, null, getMessageQueueOffset(mq), 1);
+//                } catch (MQClientException | RemotingException | InterruptedException | MQBrokerException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        break;
+//        }
+
         for (MessageQueue mq : mqs) {
             System.out.printf("Consume from the queue: %s%n", mq);
             SINGLE_MQ:
             while (true) {
                 try {
                     PullResult pullResult =
-                        consumer.pullBlockIfNotFound(mq, null, getMessageQueueOffset(mq), 32);
-                    System.out.printf("%s%n", pullResult);
-                    putMessageQueueOffset(mq, pullResult.getNextBeginOffset());
+                        consumer.pull(mq, null, getMessageQueueOffset(mq), 1);
+//                    System.out.printf("%s%n", pullResult);
+                    System.out.printf("%s %n", pullResult.getMsgFoundList());
+                    if(pullResult.getMsgFoundList() == null || pullResult.getMsgFoundList().size() == 0)
+                        break;
+
+                    String id = pullResult.getMsgFoundList().get(0).getKeys();
+
+                    // KEY+i => remove key and parse i
+                    int idd = Integer.parseInt(id.substring(3));
+
+                    System.out.printf("key is %d",idd);
+
+                    if(idd == expectedMessage) {
+                        messages.add(pullResult.getMsgFoundList().get(0));
+                        expectedMessage++;
+                        putMessageQueueOffset(mq, pullResult.getNextBeginOffset());
+                    } else {
+                        while(buffer.containsKey(expectedMessage)) {
+                            messages.add(buffer.remove(expectedMessage));
+                            expectedMessage++;
+                        }
+                        buffer.put(expectedMessage,pullResult.getMsgFoundList().get(0));
+                        putMessageQueueOffset(mq, pullResult.getNextBeginOffset());
+                    }
                     switch (pullResult.getPullStatus()) {
                         case FOUND:
                             break;
@@ -59,7 +99,10 @@ public class PullConsumer {
                 }
             }
         }
-
+        for(int i=0;i<messages.size();i++) {
+            System.out.printf("index is %d \n", i);
+            System.out.printf("message is %s \n", messages.get(i));
+        }
         consumer.shutdown();
     }
 
